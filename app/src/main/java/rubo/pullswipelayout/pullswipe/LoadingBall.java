e8f86df6a6a35b7ddff02f637f5a62847eb7e34b
zmer007 <1779964617@qq.com>
2016-06-19 16:23:34 +0800

    改善加载中动画, 但是依旧很难看

--- app/src/main/java/rubo/pullswipelayout/pullswipe/LoadingBall.java
+++ app/src/main/java/rubo/pullswipelayout/pullswipe/LoadingBall.java
@@ -7,7 +7,6 @@ import android.graphics.Paint.Style;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.animation.Animation;
-import android.view.animation.Animation.AnimationListener;
 import android.view.animation.DecelerateInterpolator;
 import android.view.animation.Transformation;
 
